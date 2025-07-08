71778605efccda363d8249d5b08bfe96ca665bd7
Rainer Eduardo Lopez <rainerdev@gmail.com>
2025-07-08 18:56:51 -0300

    refactor: migrate admin layout from SMUI to noph-ui components

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -2,9 +2,7 @@
 	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
 	import { supabase } from '$lib/supabaseClient';
-	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
-	import IconButton from '@smui/icon-button';
-	import Button from '@smui/button';
+	import { TopAppBar, Button, Icon } from 'noph-ui';
 
 	let { children } = $props();
 	let user = $page.data.user;
@@ -24,23 +22,21 @@
 </svelte:head>
 
 {#if $page.url.pathname !== '/admin/login' && $page.url.pathname !== '/admin/register'}
-	<TopAppBar variant="fixed" color="secondary">
-		<Row>
-			<Section>
-				<Title>Hauxenda Admin</Title>
-			</Section>
-			<Section align="end" toolbar>
-				{#if user}
-					<span class="hidden md:inline-block mr-4 text-sm">
-						{user.email}
-					</span>
-				{/if}
-				<Button on:click={handleLogout} variant="outlined">
-					<span class="material-icons mr-2">logout</span>
-					Sair
-				</Button>
-			</Section>
-		</Row>
+	<TopAppBar fixed color="secondary">
+		<div slot="title">Hauxenda Admin</div>
+		<div slot="actions">
+			{#if user}
+				<span class="hidden md:inline-block mr-4 text-sm">
+					{user.email}
+				</span>
+			{/if}
+			<Button on:click={handleLogout} variant="outlined">
+				{#snippet start()}
+					<Icon>logout</Icon>
+				{/snippet}
+				Sair
+			</Button>
+		</div>
 	</TopAppBar>
 
 	<main class="pt-16 p-4">
